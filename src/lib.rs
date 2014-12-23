64f5e77e149d2d2763d8db87cef4b33bffaa81ca
shockham <samuel.hockham@gmail.com>
2014-12-23 13:58:17 +0000

    added html test

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,19 @@ pub fn elem(elem_type:&str, class:&str, content:String) -> String{
     format!("<{e_type} {class}>{value}</{e_type}>",e_type=elem_type,value=content,class=class)
 }
 
+#[test]
+fn test_elem(){
+    let elem_string = elem("div", "class=\"test\"", String::from_str("content"));
+    assert!(elem_string == String::from_str("<div class=\"test\">content</div>"))
+}
+
+
 pub fn html(title:&str, content:String) -> String{
     format!("<!DOCTYPE html><head><title>{}</title></head><body>{}</body></html>", title, content)
 }
 
 #[test]
-fn test_elem(){
-    assert!(elem("div", "class=\"test\"", String::from_str("content")) == String::from_str("<div class=\"test\">content</div>"))
+fn test_html(){
+    let html_string = html("hello", String::from_str("content"));
+    assert!(html_string == String::from_str("<!DOCTYPE html><head><title>hello</title></head><body>content</body></html>"))
 }
